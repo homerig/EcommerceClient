3939766fb0d5d02f7f6336587b2de540a23ea195
LaraMigueltorena <migueltorenalara@gmail.com>
2024-10-15 12:10:26 -0300

    Users y Register

--- client/src/views/App.jsx
+++ client/src/views/App.jsx
@@ -4,6 +4,8 @@ import Navigation from "./Navigation";
 import Home from './Home';
 import Products from "./Products";
 import Login from "./Login";
+import Users from "./Users";
+import Register from "./Register";
 import { Routes,Route } from "react-router-dom";
 
 const App = () => {
@@ -14,7 +16,9 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/Products' element={<Products/>}/>
+        <Route path='/Users' element={<Users/>}/>
         <Route path='/Login' element={<Login/>}/>
+        <Route path='/Register' element={<Register/>}/>
         
       </Routes>
     </>

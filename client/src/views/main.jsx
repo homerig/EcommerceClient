6503fcc7fcdfd0bf5ee61cecc7a68882d3c5ffbe
Bárbara Gaito <127039725+BarbaraGaito@users.noreply.github.com>
2024-11-12 15:44:21 -0300

    cambios redux

--- client/src/views/main.jsx
+++ client/src/views/main.jsx
@@ -3,11 +3,23 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './css/index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { store } from "./Redux/store.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
